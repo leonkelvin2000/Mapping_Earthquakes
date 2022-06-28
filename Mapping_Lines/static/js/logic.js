5710d728b3b883cbc8e556075ddd4acd5181e353
Kelvin Leon <98680133+leonkelvin2000@users.noreply.github.com>
2022-06-28 01:38:48 -0400

    updated logic

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -8,11 +8,26 @@ let map = L.map("mapid", {
   ],
   zoom: 4
 });
+
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [30.1900, -97.6687],
+  [43.6777, -79.6248],
+  [40.6413, -73.7781]
+];
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "blue",
+  weight: 4,
+  opacity: 0.5,
+  dashArray: '5,10'
+
+}).addTo(map);
+
   
 // Get data from cities.js
 let cityData = cities;
-
-
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
   console.log(city)
@@ -32,7 +47,7 @@ cityData.forEach(function(city) {
 });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
